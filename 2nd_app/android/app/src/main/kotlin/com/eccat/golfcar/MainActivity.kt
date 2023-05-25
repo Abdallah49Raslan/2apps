b5792ff5f7ad4cbecf344e4e33a20c37b1d0c3b2
Abdallah49Raslan <abdallahraslan682@gmail.com>
2023-05-26 01:52:44 +0200

    delete 2app

--- 2nd_app/android/app/src/main/kotlin/com/eccat/golfcar/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.eccat.golfcar
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- 2nd_app/lib/controller/controller_bindings.dart
+++
@@ -1,9 +0,0 @@
-import 'package:application_1/controller/controller.dart';
-import 'package:get/get.dart';
-
-class controllerBindings extends Bindings{
-  @override
-  void dependencies() {
-    Get.put<controller>(controller());
-  }
-}
